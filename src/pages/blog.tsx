006c52f55503ce0a81def5a10d595eb05dfa1b17
Jii Eu <hello@jiieu.com>
2023-04-18 11:26:14 -0400

    [dev] Displaying posts list compeleted
    https://github.com/rolemadelen/blogv2/issues/7

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,22 +1,41 @@
+import { PostsWrapper, PostList, PostUID, PostTitle } from '@/components/custom-tw-components';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { getAllPosts } from '../../lib/blog';
 import Link from 'next/link';
 
-export default function Blog( { posts }) {
+interface Post {
+    slug: string,
+    title: string,
+    posttitle: string,
+    date: string,
+    updated: string,
+    uid: string
+};
+
+interface Posts {
+    posts: Post[]
+}
+
+const Blog: React.FC<Posts> = ({posts}) => {
+// export default function Blog( { posts }) {
     return (
         <>
             <Header />
-            {posts.map((post) => (
-                <Link
-                    key={`${post.slug}`}
-                    href={`/blog/${post.slug}`}
-                    passHref>
-                        <p>
-                            {post.uid} - {post.title}
-                        </p>
-                </Link>
-            ))}
+            <PostsWrapper>
+                <p>*글은 관련성에 따라 의도한 순서로 나열되어 있습니다. 작성 시간순이 아님을 유념해주세요.</p>
+                {posts.map((post) => (
+                    <Link
+                        key={`${post.slug}`}
+                        href={`/blog/${post.slug}`}
+                        passHref>
+                            <PostList>
+                                <PostUID>{post.uid}</PostUID>
+                                <PostTitle>{post.title}</PostTitle>
+                            </PostList>
+                    </Link>
+                ))}
+            </PostsWrapper>
             <Footer />
         </>
     )
@@ -31,3 +50,5 @@ export async function getStaticProps() {
         },
     }
 }
+
+export default Blog;
