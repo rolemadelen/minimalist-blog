89885e554b2f58b1f3f452e0f75013cbf7fdf018
rolemadelen <bprsstnt@pm.me>
2023-05-01 09:25:21 -0400

    giscus integration

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,29 +1,22 @@
-import React, { useRef } from 'react';
-import giscus from '../lib/giscus';
+import React from 'react';
+import Giscus from "@giscus/react";
 
-const Comments = () => {
-    const comment = useRef(null);
-
-    giscus({
-        url: 'https://giscus.app/client.js',
-        repo: 'rolemadelen/blogv2',
-        repoId: 'R_kgDOJXRY1A',
-        category: 'General',
-        categoryId: 'DIC_kwDOJXRY1M4CWLQD',
-        lang: 'en',
-        theme: 'preferred_color_scheme',
-        mapping: 'title',
-        crossorigin: 'anonymous',
-        loading: 'lazy',
-        inputPosition: 'top',
-        ref: comment,
-    });
-
+export default function Comments() {
     return (
-        <div className="w-full mt-[5em]">
-            <div ref={comment}></div>
-        </div>
+        <Giscus
+            repo="rolemadelen/blogv2"
+            repoId="R_kgDOJXRY1A"
+            category="General"
+            categoryId="DIC_kwDOJXRY1M4CWLQD"
+            mapping="title"
+            strict="0"
+            reactionsEnabled="1"
+            emitMetadata="0"
+            inputPosition="top"
+            theme="preferred_color_scheme"
+            llang="en"
+            loading="lazy"
+            crossorigin="anonymous"
+        />
     );
 };
-
-export default Comments;
