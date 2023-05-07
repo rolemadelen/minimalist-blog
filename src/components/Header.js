51d67fed756508d322bf1be62dadccff08f4cd2e
rolemadelen <bprsstnt@pm.me>
2023-05-07 15:33:45 -0400

    issue #20 completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,9 @@ import {
 const Header = ({lang}) => {
     return (
         <HeaderWrapper>
-            <Logo>rolemadelen</Logo>
+            <Link href={"/"} >
+                <Logo>rolemadelen</Logo>
+            </Link>
             <div>
                 <Link className={`btn ${lang === 'en' ? 'btn-ghost' : ''}`} href="/blog/ko">한</Link>
                 <Link className={`btn ${lang === 'ko' ? 'btn-ghost' : ''}`} href="/blog/en">A</Link>
