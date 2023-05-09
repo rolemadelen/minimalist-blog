18be47eed8ab950d966d4802b8bafba5118d5cc5
Jii Eu <hello@jiieu.com>
2023-05-09 14:30:13 -0400

    updated date added

--- src/pages/blog/ko/[...id].js
+++ src/pages/blog/ko/[...id].js
@@ -19,7 +19,7 @@ export default function Post({ post }) {
         <ArticleHeader>
             <ArticleTitle>{post.title}</ArticleTitle>
             <div className={'flex flex-col'}>
-                <ArticleDate>{new Date(post.date).toLocaleDateString('en-US', { timeZone: 'UTC', month: 'short', day: 'numeric', year: 'numeric' })}</ArticleDate>
+                <ArticleDate>{new Date(post.updated ? post.updated : post.date).toLocaleDateString('en-US', { timeZone: 'UTC', month: 'short', day: 'numeric', year: 'numeric' })}</ArticleDate>
                 <Link href='/blog/ko' className={'w-[2rem] h-[1.5625rem] flex justify-center items-center rounded mb-[1.25rem] border-[1px] border-solid border-[#ccc] hover:bg-[#e7e7e7] duration-75'}>
                     <Image src={IconArrowLeft} alt="←" width="auto"/>
                 </Link>
