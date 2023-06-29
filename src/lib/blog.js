f452f5141a0744a7ca0db076451d83687a42d7e7
Jii Eu <hello@jiieu.com>
2023-06-29 05:50:03 -0400

    codeblock theme changed & posts names modified

--- src/lib/blog.js
+++ src/lib/blog.js
@@ -1,19 +1,19 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import { remark } from 'remark';
-import html from 'remark-html';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
 
 export function getAllPosts() {
   const postDirectory = path.join(process.cwd(), `/src/post`);
   const fileNames = fs.readdirSync(postDirectory);
 
   const allpostData = fileNames.map((fileName) => {
-    const slug = fileName.replace(/\.md$/, '');
+    const slug = fileName.replace(/\.md$/, "");
 
     // Read markdown file as string
     const fullPath = path.join(postDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const fileContents = fs.readFileSync(fullPath, "utf8");
 
     // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents);
@@ -23,13 +23,13 @@ export function getAllPosts() {
     };
   });
 
-  // sort post by most recent only for the Blog 
+  // sort post by most recent only for the Blog
   allpostData.sort((postA, postB) => {
     let dateA = new Date(postA.date);
     let dateB = new Date(postB.date);
 
     return dateB - dateA;
-  })
+  });
 
   return allpostData;
 }
@@ -40,7 +40,7 @@ export function getAllPostIds() {
   return fileNames.map((fileName) => {
     return {
       params: {
-        id: [fileName.replace(/\.md$/, '')],
+        id: [fileName.replace(/\.md$/, "")],
       },
     };
   });
@@ -49,7 +49,7 @@ export function getAllPostIds() {
 export async function getPostData(id) {
   const postDirectory = path.join(process.cwd(), `/src/post/`);
   const fullPath = path.join(postDirectory, `${id[0]}.md`);
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const fileContents = fs.readFileSync(fullPath, "utf8");
 
   // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents);
--- src/lib/codeblock.js
+++ src/lib/codeblock.js
@@ -1,40 +1,43 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import remarkGfm from 'remark-gfm';
-import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import React from "react";
+import remarkGfm from "remark-gfm";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import {
+  vscDarkPlus,
+  vs,
+} from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 class Preview extends React.Component {
-    render() {
-        return (
-            <ReactMarkdown
-                remarkPlugins={[remarkGfm]}
-                components={{
-                    code({ inline, className, children, ...props }) {
-                        const match = /language-(\w+)/.exec(className || '');
-                        return !inline && match ? (
-                            <SyntaxHighlighter
-                                style={vscDarkPlus}
-                                language={match[1]}
-                                showLineNumbers={false}
-                                PreTag='div'
-                                {...props}>
-                                {String(children).replace(/\n$/, '')}
-                            </SyntaxHighlighter>
-                        ) : (
-                            <code
-                                className={className}
-                                {...props}>
-                                {children}
-                            </code>
-                        );
-                    },
-                }}>
-                {this.props.markdown}
-            </ReactMarkdown>
-        );
-    }
+  render() {
+    return (
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={vs}
+                language={match[1]}
+                showLineNumbers={false}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {this.props.markdown}
+      </ReactMarkdown>
+    );
+  }
 }
 
 export default Preview;
