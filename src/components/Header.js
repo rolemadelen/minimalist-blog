3dca5481ee8f936108365405f09e6491a97c63fa
Jii Eu <hello@jiieu.com>
2023-06-29 21:41:41 -0400

    UI updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="max-w-[36rem] m-auto text-3xl mt-20 mb-10 px-6">
+    <div className="max-w-[36rem] m-auto text-3xl mt-20 mb-16 px-6">
       <Link href={"/about"}>
         <div>je.</div>
       </Link>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,5 +8,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
