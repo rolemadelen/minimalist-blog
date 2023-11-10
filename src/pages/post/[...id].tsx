0bbf95991bdf828e38845cd65c1f544c57e2e9b3
Jii Eu <hello@jiieu.com>
2023-11-10 04:26:38 -0500

    TOC functionality added

--- src/pages/post/[...id].tsx
+++ src/pages/post/[...id].tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import Preview from '@/lib/codeblock'
 import { getAllPostIds, getPostData } from '@/lib/blog'
 import Link from 'next/link'
@@ -17,11 +17,34 @@ interface IPost {
 }
 
 const Post: React.FC<IPost> = ({ post: { title, date, markdown } }) => {
+  const toc = markdown.split('\n').filter((line) => line.trim().startsWith('#'))
   const formattedDate = new Date(date).toLocaleDateString('en-us', {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
   })
+
+  const getHeadingLevel = (str: string) => {
+    const match = str.match(/^#+/)
+    if (match) {
+      const count = match[0].length
+      return count
+    }
+
+    return 0
+  }
+
+  const createTOC = () => {
+    let listItems = ''
+    toc.forEach((item) => {
+      let headLevel = `h-${getHeadingLevel(item)}`
+      item = item.replace('#', '')
+      item = item.replaceAll('#', '　')
+      listItems += `<li class="${headLevel} text-sm !list-none !left-0">${item}</li>`
+    })
+
+    return `<ul class="max-w-[20rem] hidden lg:block lg:fixed left-[2vw] top-0 mt-52">${listItems}</ul>`
+  }
   return (
     <>
       <Head>
@@ -31,8 +54,9 @@ const Post: React.FC<IPost> = ({ post: { title, date, markdown } }) => {
         <meta content={title} property="twitter:description" />
       </Head>
       <ProgressBar />
+
       <div className="post max-w-[40rem] m-auto px-6">
-        <div className="post-title text-3xl mb-3 mt-36 leading-normal">
+        <div className="post-title text-4xl mb-3 mt-36 leading-normal">
           {title}
         </div>
         <div className="flex justify-between items-center mb-20">
@@ -43,6 +67,10 @@ const Post: React.FC<IPost> = ({ post: { title, date, markdown } }) => {
             </div>
           </Link>
         </div>
+        <div
+          className="toc"
+          dangerouslySetInnerHTML={{ __html: createTOC() }}
+        ></div>
         <div className="post-content mb-10 pb-10 border-b">
           <Preview markdown={markdown} />
         </div>
