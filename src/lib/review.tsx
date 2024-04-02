0ec16076b236e779964fc83bc963afeb68f285cd
Madelen Yoo <rolemadelen@tuta.io>
2024-04-01 21:57:28 -0400

    review updated

--- src/lib/review.tsx
+++ src/lib/review.tsx
@@ -1,5 +1,5 @@
 export type Data = {
-  product: string
+  name: string
   date: string
   review: string
   rating: number
@@ -10,26 +10,44 @@ export type Data = {
 export type ReviewData = {
   snacks: Data[]
   media: Data[]
+  movies: Data[]
 }
 
 export const reviewed: ReviewData = {
   snacks: [
     {
-      product: 'QUAKER - Chocolate Rice Crips',
+      name: 'QUAKER - Chocolate Rice Crips',
       date: '2024/03/30',
-      review: 'the more you eat, the sweeter it gets',
-      rating: 4,
-      image: 'quaker-chocolate-rice-crisps.png',
+      review: 'the more you eat, the sweeter it gets.',
+      rating: 3.5,
+      image: 'quaker-choco-rice-crisps.png',
     },
   ],
   media: [
     {
-      product: 'Node.js: The Documentary | An origin story',
+      name: 'Node.js: The Documentary | An origin story',
       date: '2024/03/23',
-      review: "I didn't know there were so many dramas behind node.js",
+      review: "I didn't know there were so many dramas behind node.js.",
       link: 'https://youtu.be/LB8KwiiUGy0?si=4Fe4CRKDmB_VJqEm',
       rating: 5,
       image: 'honeypot.jpeg',
+    },
+  ],
+  movies: [
+    {
+      name: '「M3GAN」 directed by Gerald Johnstone',
+      date: '2024/04/01',
+      review:
+        'a glimpse into potential dystopian future resulting from technological singularity.',
+      rating: 4,
+      image: 'm3gan.jpeg',
+    },
+    {
+      name: "「Five Nights at Freddy's」 directed by Emma Tammi",
+      date: '2024/03/30',
+      review: 'Alphonse Elric brainwashed by the yellow bunny to harm others.',
+      rating: 3,
+      image: '5nights_at_freddys.jpg',
     },
   ],
 }
