67a4258ada6489f610cf9893798dcf0ce1071f03
rolemadelen <rolemadelen17@pm.me>
2024-04-06 17:42:19 -0400

    header logo modifed on scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Link from 'next/link'
 import styles from './Header.module.scss'
 
 const Header = () => {
+  const headerRef = useRef<HTMLHeadElement>(null)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (scrollY > 80) {
+        headerRef.current?.classList.add(`${styles.scroll}`)
+      } else {
+        headerRef.current?.classList.remove(`${styles.scroll}`)
+      }
+    })
+  }, [])
+
+  const moveToTop = () => {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+  }
   return (
-    <header className={styles.header}>
+    <header className={styles.header} ref={headerRef}>
+      <div className={styles.top} onClick={moveToTop}>
+        <span className={styles.earLeft}>૮</span>
+        <span className={styles.face}>• ᴥ •</span>
+        <span className={styles.earRight}>ა</span>
+      </div>
       <div className={styles.logo}>
         <Link href="/">
           <h1>
@@ -12,6 +33,7 @@ const Header = () => {
             <span className={styles['logo__text']}>Madelen</span>
             <span className={styles['logo__cursor']}></span>
           </h1>
+          <h1></h1>
         </Link>
       </div>
       <nav className={styles.nav}>
